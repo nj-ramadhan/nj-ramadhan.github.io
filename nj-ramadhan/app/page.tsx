efefc14105e96771662f369b448e889a07ae443d
NJ-Ramadhan <informatics.nj.ramadhan@gmail.com>
2026-02-19 13:00:02 +0700

    Update Display

--- nj-ramadhan/app/page.tsx
+++ nj-ramadhan/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <main className="flex min-h-screen w-full flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
       <script>window.staDarkLightChooser = true;</script>
       <script>const isSiteThemeDark = false;</script>
       <script src="/js/load-theme.js"></script>
