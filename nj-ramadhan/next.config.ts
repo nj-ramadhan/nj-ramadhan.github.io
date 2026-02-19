1e1d8409c4d466468da240fff4d6574e11b65f9a
NJ-Ramadhan <informatics.nj.ramadhan@gmail.com>
2026-02-19 12:42:17 +0700

    Migrate to Next.js and add deployment workflow

---
+++ nj-ramadhan/next.config.ts
@@ -0,0 +1,13 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  output: 'export', 
+  images: {
+    unoptimized: true,
+  },  
+  // basePath: '/nj-ramadhan'
+
+};
+
+export default nextConfig;
